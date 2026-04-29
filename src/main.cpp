644ef40f94d4b36c22d6edd61fd0a448cbef9248
Leonardo Quinones <leonardo.quinones@unity3d.com>
2026-04-29 18:16:46 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,42 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+
 int main()
 {
-    // Creation de fenetree
+    // Create a window
+    sf::Window window;
+
+    window.create(sf::VideoMode({800, 600}), "Mon super project SFML 3");
+
+    window.setTitle("Mon jeux video SFML 3");
+
+    // Loop that continues until we close the window
+    while (window.isOpen())
+    {
+    }
+
+    return 0;
 }
+
+// Video 7 SFML FR
+/*
+This is a way to create a windows
+// Create a window
+    sf::Window window;
+
+    window.create(sf::VideoMode({800, 600}), "Mon super project SFML 3");
+
+    window.setTitle("Mon jeux video SFML 3");
+
+    // Loop that continues until we close the window
+    while (window.isOpen())
+    {
+    }
+
+    return 0;
+*/
 
 // SFML 3 Sample teste
 /*
